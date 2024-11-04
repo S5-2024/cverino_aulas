54a2be6d6ed84ab5870e2aa4614f558f21947650
gabriellesote <gabrielle.sote@gmail.com>
2024-11-04 20:19:12 -0300

    aula API Covid

---
+++ aula7/covid.py
@@ -0,0 +1,26 @@
+import requests
+import webbrowser
+import json
+
+country = "bra"
+
+url = f"https://disease.sh/v3/covid-19/countries/{country}"
+
+response = requests.get(url)
+dados = response.json()
+
+if(response.status_code == 200):
+  data = response.json()
+
+  pais = data.get("country")
+  deaths = data.get("deaths")
+  hoje = data.get("todayCases")
+  print(f"Covid no {pais}:    - mortes:{deaths}  - Casos de hoje:{hoje} ")
+
+#Abrir em página
+  # with open('response.json','w') as json_file:
+  #   json.dump(data,json_file,indent=4)
+
+  #   webbrowser.open('response.json')
+else:
+  print(response.status_code)
